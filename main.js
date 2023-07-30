2215d94a3c71978553f14733d15ae82f6de72980
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2023-07-30 19:24:44 -0300

    transporte de arquivos

--- main.js
+++ main.js
@@ -1,44 +1,137 @@
-const form = document.forms.entrada;
-form.addEventListener('submit', multiplicar);
-form.limpar.addEventListener('click', remove);
-var multiplica =[];
+import { getLocais, getLocais2, getEmbaralhar} from "./tabuleiro.js";
 
+const botao = document.getElementById("START");
+const htmlJogadorLocais = document.querySelector(".jogadorLocais");
+const htmlComputadorLocais = document.querySelector(".computadorLocais");
+const htmlCartas = document.querySelector(".cartas");
+const htmlVisor = document.querySelector(".visor");
+const htmlRound = document.querySelector(".round");
 
+let jogador = {
+    energia:1,
+    mao: []
+};
 
-function multiplicar(evento){
-    console.log("incionou calculo");
-    evento.preventDefault();
-    var vl1 = document.getElementById("a").value;
-    var vl2 = document.getElementById("b").value;
-    var vl3 = document.getElementById("c").value;
-    var cal = parseFloat(vl1) * parseFloat(vl2) * parseFloat(vl3);
-    adicionar(cal);
-    imprimeLista();
-    
-    a.value="";
-    b.value="";
-    c.value="";
- 
+let arrastado = null;
+
+botao.onclick = function comecarJogo(){
+   
+    let baralho = getEmbaralhar();
+
+    setup();
+    atualizar(mao(4, baralho)); 
+    rodada(jogador.energia);
+    visor(baralho.length,jogador.energia);
+       
 }
 
-function imprimeLista(){
-   var ul = document.querySelector("ul");
-   ul.innerHTML="";
-    
-    for(var i=0; i<multiplica.length;i++){
-        var lista = document.getElementById("listaElementos");
-        var li = document.createElement("li");
-        li.textContent = multiplica[i];
-        lista.appendChild(li);
-        console.log(multiplica[i]);
+function visor(quantBaralho,round){
+    htmlVisor.innerHTML = "CARTAS:"+quantBaralho+'</br>'+"ENERGIA:"+round;
+}
+
+function rodada(round){
+    htmlRound.innerHTML = round+"º"+" ROUND";
+}
+
+//function atualizar(posicao){
+   // console.log("passou aqui");
+//}
+
+function arrastar(evento){
+    arrastado = evento.target
+}
+
+function passa(evento){
+    evento.preventDefault();
+}
+
+function setup(){
+    const locais = getLocais();
+    const locais2 = getLocais2();
+    for(let i=0; i<locais.length;i++){
+        const local = locais[i];
+        const local2 = locais2[i];
+        const localCriado = criaLocalJogador(local);
+        const localCriado2 = criaLocalComputador(local2);
+        htmlJogadorLocais.appendChild(localCriado);
+        htmlComputadorLocais.appendChild(localCriado2);
     }
 }
 
-function adicionar(cal){
-    multiplica.push(cal);
+function mao(cartas, baralho){
+    
+    for(let i=0; i<cartas;i++){     
+       criaCarta(i, baralho);
+       jogador.mao[i] = baralho[i];
+       jogador.mao[i].indice = i;
+    }
+
+    
 }
 
-function remove(){
-    multiplica.pop();
-    imprimeLista();
+function criaCarta(i, baralho){
+    
+    const cartaCriada = document.createElement('div');
+    cartaCriada.dataset.posicao = i;
+    cartaCriada.innerHTML = `${baralho[i].nome +'</br>'+ 'Energia:'+baralho[i].energia +'</br>'+'Força:'+ baralho[i].forca}`;
+    cartaCriada.className = "carta";
+    htmlCartas.appendChild(cartaCriada);
+    cartaCriada.draggable = true;
+    cartaCriada.addEventListener('dragstart', arrastar);
+
+    return cartaCriada;
+}
+
+
+function criaLocalComputador(local){
+    const localCriado = document.createElement('div');
+    localCriado.dataset.posicao = local;
+    localCriado.className = 'localComputador';
+    localCriado.classList.add('localComputador');
+    localCriado.addEventListener('dragover', PassouPorCima);
+    localCriado.addEventListener('drop', recebe);
+ 
+    return localCriado;    
+}
+
+function criaLocalJogador(local){
+    const localCriado = document.createElement('div');
+    localCriado.dataset.posicao = local;
+    localCriado.className = 'localJogador';
+    localCriado.classList.add('localJogador');
+    localCriado.addEventListener('dragover', PassouPorCima);
+    localCriado.addEventListener('drop', recebe);
+ 
+    return localCriado;    
+}
+
+function recebe(evento){
+     
+    if(arrastado&&evento.target.classList.contains('localJogador')){//&&jogador.mao[arrastado.dataset.posicao].energia<=jogador.energia){ 
+        
+        console.log(arrastado.dataset.posicao);
+       // jogador.energia = jogador.mao[arrastado.dataset.posicao].energia - jogador.energia;
+
+        if(parseInt(arrastado.dataset.posicao)<3){
+    
+            for(let i=parseInt(arrastado.dataset.posicao); i<jogador.mao.length;i++){
+    
+                jogador.mao[i]=jogador.mao[i+1];
+
+            }
+            jogador.mao.pop();
+        }else{  
+            jogador.mao.pop();
+        }
+        evento.target.appendChild(arrastado);
+
+        if(jogador.energia==0){
+            console.log("paassou");
+        }
+          
+    }         
+}
+
+function PassouPorCima(evento){
+    evento.preventDefault();
 }
